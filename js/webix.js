821271f3521c4c8c50f11c1b803aa920490a36e1
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-03-01 10:26:53 +0300

    add two methods for wi.dt

--- js/webix.js
+++ js/webix.js
@@ -1,2 +1,5 @@
 //Iterate over all rows in the table
-ui.datatable(function (row) { }, [boolean all]) //row is id of the row.
+ui.datatable(function (row) { }, [boolean all]) //row is ID of the row.
+//
+ui.datatable.getItem(row) // row is ID of the row.
+ui.datatable.updateItem(row_id, item) // item is object contains data for the row
