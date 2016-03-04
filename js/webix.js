1fd5b0c699c95217b55247c5b7eca76741153da9
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-03-04 11:13:16 +0300

    Example init config DataTable

--- js/webix.js
+++ js/webix.js
@@ -3,3 +3,24 @@ ui.datatable(function (row) { }, [boolean all]) //row is ID of the row.
 //
 ui.datatable.getItem(row) // row is ID of the row.
 ui.datatable.updateItem(row_id, item) // item is object contains data for the row
+//Example init config DataTable
+ui({
+    view: 'datatable',
+    container: '',
+    columns: [
+        {
+            id: '',
+            header: [//or string
+                {
+                    text: '',
+                    content: 'nameFilter',
+                    css: {}// or string
+                }    
+            ],
+            sort: 'string',
+            template: function (obj) {
+                report obj.id;
+            }
+        }    
+    ]
+})
