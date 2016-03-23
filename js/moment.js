928cafde80e46c3d8c9f13c393661375e81cd4be
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-03-23 15:24:30 +0300

    learned several methods for moment.js

--- js/moment.js
+++ js/moment.js
@@ -2,3 +2,9 @@ moment().format('MM/YYYY'); //get current month and year
 moment('not is a re).isValit();
 moment().endOf().format('DD/MM/YYYY');
 moment().startOf().format('DD/MM/YYYY');
+moment().month();
+moment().month(2);
+moment().day();
+moment().add(2, 'day');
+moment().subtract(2, 'month');
+moment().format('MMMM');
