955d7b28d8c75348a62a38b4f2080703801381bf
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-04-06 15:14:31 +0300

    Segment uri

--- php/ci2.php
+++ php/ci2.php
@@ -3,3 +3,6 @@
 //  Connecting to your DB
 //  Connectivity settings, as defined in your database config file will be used
 $this->load->model('model_name', '', TRUE);
+
+//Get segment path from uri
+$this->uri->segment($int); 
