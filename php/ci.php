48486d4331652103bd3404c1758df26b6518d477
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-04-18 16:03:53 +0300

    Use paginator within CI

---
+++ php/ci.php
@@ -0,0 +1,19 @@
+<?php
+//Models
+//  Connecting to your DB
+//  Connectivity settings, as defined in your database config file will be used
+$this->load->model('model_name', '', TRUE);
+
+//Get segment path from uri
+$this->uri->segment($int); 
+
+//Use paginator
+$this->load->library('pagination');
+
+$config['base_url'] = 'http://example.com/index.php/test/page/';
+$config['total_rows'] = 200;
+$config['per_page'] = 20;
+
+$this->pagination->initialize($config);
+
+echo $this->pagination->create_links();
