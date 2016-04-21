d9db4f1acf0270476bb18771cc9648e8295b5337
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-04-21 16:40:46 +0300

    Create countries.sql

---
+++ sql/countries.sql
@@ -0,0 +1,12 @@
+-- Вывод всех строк из таблиц 
+SELECT *
+    FROM `country_settings`
+    INNER JOIN `country` 
+    ON `country_settings`.id_country = `country`.id;
+    
+-- Выборка для списка параметров по странам
+SELECT *
+    FROM `country_settings`
+    INNER JOIN `country` 
+    ON `country_settings`.id_country = `country`.id
+    ORDER BY `country_settings`.id_country ASK;
