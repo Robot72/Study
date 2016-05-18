33e5a94024945e91dcb5689598426d139d32ae56
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-05-18 11:20:25 +0300

    Update countries.sql
    Запрос на выборку стран с кодами.

--- sql/countries.sql
+++ sql/countries.sql
@@ -10,3 +10,9 @@ SELECT *
     INNER JOIN `country` 
     ON `country_settings`.id_country = `country`.id
     ORDER BY `country_settings`.id_country;
+    
+-- Выборка всех стран с их символьными и телефонными кодами    
+SELECT `id_country`, `value`, `code`
+FROM `country_settings` INNER JOIN `country` 
+ON `country_settings`.id_country = `country`.id
+WHERE `key`='phone_code' AND `public`=1;
