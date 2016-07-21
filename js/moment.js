c90afd630c29e5411f9503f30ce86e723f827ba6
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-07-21 08:59:10 +0300

    Update moment.js

--- js/moment.js
+++ js/moment.js
@@ -1,5 +1,5 @@
 moment().format('MM/YYYY'); //get current month and year
-moment('not is a re).isValit();
+moment('not is a re').isValit();
 moment().endOf().format('DD/MM/YYYY');
 moment().startOf().format('DD/MM/YYYY');
 moment().month();
