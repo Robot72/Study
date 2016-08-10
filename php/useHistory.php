41f2bc56d111aaa9623aa79fa70acbf657076d4d
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-08-10 16:22:49 +0300

    PHP. UseHistory. How to get file permissions.

--- php/useHistory.php
+++ php/useHistory.php
@@ -15,11 +15,14 @@ array_fill_keys(['key1', 'key2'], 'value'); //: array
 date('Y:m:d H:i:s');
 
 header('Content-Type: text/html; charset=utf-8;');
+
+/* STRINGs */
 serialize($obj);
 unserialize($str);
-
 strtolower($str);
 intval($value);
 
+/* FILES */
 file_get_content($file);
 file_put_content($file);
+substr(sprintf('%o', fileperms('filename')), -3); //Get permissions code for access to perpose file
