84b7f914b5b3f042c022a207b5653d71c9fa681b
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-08-16 10:55:43 +0300

    Func for js

--- js/useHistory.js
+++ js/useHistory.js
@@ -6,3 +6,4 @@ var Obj = {
 var arrPropsOfTheObj = Object.keys(Obj);
 arrPropsOfTheObj = Object.keys(Obj).sort();
 var scrolled = window.pageYOffset || document.documentElement.scrollTop; //It's expression will know distance scrolling top window(display).
+var arr = "a,b,c".split(','); // arr = ['a', 'b', 'c']
