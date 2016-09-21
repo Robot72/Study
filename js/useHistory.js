9e57e716435fa9d28732a36483aa2cef3c877051
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-09-21 10:10:17 +0300

    Update useHistory.js

--- js/useHistory.js
+++ js/useHistory.js
@@ -7,3 +7,5 @@ var arrPropsOfTheObj = Object.keys(Obj);
 arrPropsOfTheObj = Object.keys(Obj).sort();
 var scrolled = window.pageYOffset || document.documentElement.scrollTop; //It's expression will know distance scrolling top window(display).
 var arr = "a,b,c".split(','); // arr = ['a', 'b', 'c']
+'Good day'.indexOf('day');
+if( ~str.indexOf('search') ) { } // Beauty call
