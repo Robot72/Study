59a819599397433aaa43d0de0522ce7805502c87
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-10-11 12:30:23 +0300

    Create labs.php

---
+++ php/labs.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$eng_string = 'abcdefghijklmnoprst';
+
+$str_length = strlen($eng_string);
+
+$output_string = '';
+
+for($i = 0; $i < $str_length; $i += 3) {
+	
+	if($i + 3 <= $str_length) {
+		$output_string .= substr($eng_string, $i, 2);
+		$output_string .= '$';
+	} else {
+		$output_string .= substr($eng_string, $i);
+	}
+	
+}
+
+echo $output_string;
