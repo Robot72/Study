a7b8d3510ba7eea92a287dd50ea141cb620e954b
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-10-16 22:29:13 +0300

    Update yii2.php

--- php/yii2.php
+++ php/yii2.php
@@ -1,3 +1,9 @@
 //Yii::$app->formatter is object of the class yii\i18n\Formatter;
 Yii::$app->formatter->asDate($string, $formatt);
 Yii::$app->formatter->asTime($string);
+
+
+/**
+ * Command line. Helpful examples:
+ */
+compser require bower-asset/startbootstrap-sb-admin-2
