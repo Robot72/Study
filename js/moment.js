95f4b2355fc189c26ef5bc0673222c74fceb7041
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-10-24 16:36:48 +0300

    Use moment js

--- js/moment.js
+++ js/moment.js
@@ -8,3 +8,8 @@ moment().day();
 moment().add(2, 'day');
 moment().subtract(2, 'month');
 moment().format('MMMM');
+
+//Differerent duration
+var from = moment(params.dateFrom, 'DD.MM.YYYY');
+var to = moment(params.dateTo, 'DD.MM.YYYY');                
+console.log( moment.duration(to.diff(from)).asDays() );
