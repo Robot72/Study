f0f923fd6bf78f5e3e60963fce2f14f9fe100fa7
Robert Kuznetsov <Robot72@users.noreply.github.com>
2016-11-25 10:15:58 +0300

    Update strategy.php

--- php/patterns/strategy.php
+++ php/patterns/strategy.php
@@ -1,1 +1,47 @@
 // https://habrahabr.ru/post/176505/
+
+
+<?php
+
+abstract class InitStrategy 
+{
+	
+	public abstract function make();	
+	
+}
+
+class FullLogInitStrategy extends InitStrategy 
+{
+	
+	public function make()
+	{
+		
+	}
+	
+}
+
+class RapidInitStrategy extends InitStrategy
+{
+	
+	public function make()
+	{
+		
+	}
+	
+}
+
+
+class StartApplication 
+{
+
+	if($user->os == 'Windows XP') {
+		$appTrigger = new RapidInitStrategy();
+		$appTrigger->make();
+	}
+	
+	if($user->os == 'Mac OS') {
+		$appTrigger = new FullLogInitStrategy();
+		$appTrigger->make();
+	}
+	
+}
