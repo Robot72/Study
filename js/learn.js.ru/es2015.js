14a0e0a517f7a925d95f4820057158a00e729923
Robert Kuznetsov <robot3@xaker.ru>
2017-01-17 12:20:45 +0300

    Learn js. Recent features: functions.

--- js/learn.js.ru/es2015.js
+++ js/learn.js.ru/es2015.js
@@ -13,4 +13,39 @@ let person = {
 let [des1, des2, ...desAll] = arr1;
 let {firstname: v} = person;
 
-console.log(desAll);
+/**
+ * Functions
+ */
+
+function showName(firstname = 'Petrov', secondname = 'Alex', ...additional) {
+    alert(additional);
+    alert(firstname + ' ' + secondname + ' ' + additional);
+}
+
+let arr2 = [1,2,3,4,5,6,7,8,9];
+
+let maxValue = Math.min(...arr2);
+
+let expression1 = (a, b, c) => a * b + c
+
+function defer(f, ms) {
+  return function() {
+    setTimeout(() => f.apply(this, arguments), ms)
+  }
+}
+
+function sayHi(who) {
+  alert('Привет, ' + who);
+}
+
+let sayHiDeferred = defer(sayHi, 2000);
+
+function defer(f, ms) {
+  return function() {
+    let args = arguments;
+    let ctx = this;
+    setTimeout(function() {
+      return f.apply(ctx, args);
+    }, ms);
+  }
+}
