986850d6688cf18b2d0959b25454ab417e67b194
Robert Kuznetsov <robot3@xaker.ru>
2017-05-31 17:06:46 +0300

    LearnJS. Try include babel.

--- js/learn.js.ru/webpack/webpack.config.js
+++ js/learn.js.ru/webpack/webpack.config.js
@@ -5,35 +5,42 @@ const NODE_ENV = 'development';
 const webpack = require('webpack');
 
 module.exports = {
-  entry: "./home", // string | object | array
+	entry: "./home", // string | object | array
 
-  output: {
-    // options related to how webpack emits results
+	output: {
+		// options related to how webpack emits results
 
-    // path: path.resolve(__dirname, "dist"), // string
-    // the target directory for all output files
-    // must be an absolute path (use the Node.js path module)
+		// path: path.resolve(__dirname, "dist"), // string
+		// the target directory for all output files
+		// must be an absolute path (use the Node.js path module)
 
-    filename: "bundle.js", // string
-    // the filename template for entry chunks
+		filename: "bundle.js", // string
+		// the filename template for entry chunks
 
-    // publicPath: "/assets/", // string
-    // the url to the output directory resolved relative to the HTML page
+		// publicPath: "/assets/", // string
+		// the url to the output directory resolved relative to the HTML page
 
-    library: "home", // string,
-    // the name of the exported library
+		library: "home", // string,
+		// the name of the exported library
 
-    // libraryTarget: "umd", // universal module definition
-    // the type of the exported library
+		// libraryTarget: "umd", // universal module definition
+		// the type of the exported library
 
-    /* Advanced output configuration (click to show) */
-  },
-  watch: NODE_ENV == 'development',
-  watchOptions: {
-      aggregateTimeout: 100
-  },
-  devtool: NODE_ENV == 'development' ? 'source-map' : 'nosources-source-map',
-  plugins: [
-      new webpack.EnvironmentPlugin('NODE_ENV')
-  ]
+		/* Advanced output configuration (click to show) */
+	},
+	watch: NODE_ENV == 'development',
+	watchOptions: {
+		aggregateTimeout: 100
+	},
+	devtool: NODE_ENV == 'development' ? 'source-map' : 'nosources-source-map',
+	plugins: [
+		new webpack.EnvironmentPlugin('NODE_ENV')
+	],
+	module: {
+		rules: [{
+			test: /\.js$/,
+			loader: 'babel-loader'
+		}]
+	}
+
 }
